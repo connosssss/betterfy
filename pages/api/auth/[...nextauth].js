ef4882a26d87a4e96052e1f8c38e8744a143a37b
retekant <retekant@gmail.com>
2025-02-01 11:57:18 -0800

    login working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,41 +1,18 @@
-import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
-
-const scope = "user-read-email user-top-read"; 
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
 
 export default NextAuth({
-
+  
   providers: [
     SpotifyProvider({
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      authorization: {
-        url: "https://accounts.spotify.com/authorize",
-        params: { scope },
-      },
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     }),
   ],
 
-  secret: process.env.NEXTAUTH_SECRET,
-
   callbacks: {
-
-    async jwt({ token, account }) {
-        if (account) {
-            token.id = account.id;
-            token.expires_at = account.expires_at;
-            token.accessToken = account.access_token;
-          }
-          return token;
-      },
-    
-      async session({ session, token }) {
-        session.accessToken = token.accessToken;
-        return session;
-      },
-
-      pages: {
-    signIn: "/login",
-  },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
   },
 });
