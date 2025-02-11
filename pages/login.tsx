ba131c55c1199d6f359d84dfe2a5f075b6cdabb3
retekant <retekant@gmail.com>
2025-02-11 06:46:20 -0800

    Updated Login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,22 @@
-"use client"
 import { signIn } from "next-auth/react";
 
 export default function Login() {
-
-    const handleLogin = () => {
-      signIn("spotify", { callbackUrl: "http://localhost:3000" });
-    };
+  const handleLogin = () => {
+    signIn("spotify", { callbackUrl: "http://localhost:3000" });
+  };
   
-    return (
-        <div className="flex items-center justify-center w-full h-screen bg-black">
-          <button
-            className="bg-slate-800 w-1/6 "
-            onClick={handleLogin}>
-            Login
-          </button>
-        </div>
-    );
-  }
+  return (
+    <div className="flex items-center justify-center w-full h-screen animate-background  ">
+      <div className="bg-black bg-opacity-40 h-1/2 w-1/2 flex flex-col rounded-3xl text-center justify-start items-center gap-10">
+      <h1 className="font-atkinson-hyperlegible text-6xl mt-20 ">betterfy</h1>
+      <p className=" font-atkinson-hyperlegible text-lg">An improved UI for the spotify player</p>
+      <button
+        onClick={handleLogin}
+        className=" text-lg font-semibold text-white  bg-black bg-opacity-40 hover:bg-opacity-20 rounded-lg  transition-all duration-300 font-atkinson-hyperlegible px-5 py-4
+        w-1/3">
+        Login with Spotify
+      </button>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,39 @@ export default {
         'atkinson-hyperlegible': ['"Atkinson Hyperlegible"', 'sans-serif'],
         'comic-sans': ['"Comic Sans MS"', 'cursive'],
       },
+      animation: {
+        'background': 'background 30s ease infinite',
+      },
+      keyframes: {
+        'background': {
+          '0%, 100%': {
+            'background-color': '#fca5a5', 
+          },
+          '12.5%': {
+            'background-color': '#ffd1dc',  
+          },
+          '25%': {
+            'background-color': '#fdba74',  
+          },
+          '37.5%': {
+            'background-color': '#fffacd',  
+          },
+          '50%': {
+            'background-color': '#bef264',
+          },
+          '62.5%': {
+            'background-color': '#88d8c0',  
+          },
+          '75%': {
+            'background-color': '#93c5fd', 
+          },
+          '87.5%': {
+            'background-color': '#e6e6fa',  
+          }
+        },
+      },
     },
   },
   plugins: [],
 } satisfies Config;
+
