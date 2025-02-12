e3d79ce0a20d2e34c2312fd33529b3c2895f6410
retekant <retekant@gmail.com>
2025-02-11 19:48:27 -0800

    Revert "Merge branch 'main' of https://github.com/retekant/betterfy"
    This reverts commit 65e8d9f13df6cfde4d36b97271cc10fa78bb4a1d, reversing
    changes made to 6068e52fbd11832142137802d639346f0c246f1a.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,6 @@
 import { signIn } from "next-auth/react";
 
-<<<<<<< HEAD
 
-=======
->>>>>>> ba131c55c1199d6f359d84dfe2a5f075b6cdabb3
 export default function Login() {
   const handleLogin = () => {
     signIn("spotify", { callbackUrl: "http://localhost:3000" });
@@ -11,11 +8,7 @@ export default function Login() {
   
   return (
     <div className="flex items-center justify-center w-full h-screen animate-background  ">
-<<<<<<< HEAD
       <div className="bg-black bg-opacity-40 h-2/3 w-1/2 flex flex-col rounded-3xl text-center justify-start items-center gap-10">
-=======
-      <div className="bg-black bg-opacity-40 h-1/2 w-1/2 flex flex-col rounded-3xl text-center justify-start items-center gap-10">
->>>>>>> ba131c55c1199d6f359d84dfe2a5f075b6cdabb3
       <h1 className="font-atkinson-hyperlegible text-6xl mt-20 ">betterfy</h1>
       <p className=" font-atkinson-hyperlegible text-lg">An improved UI for the spotify player</p>
       <button
@@ -24,7 +17,6 @@ export default function Login() {
         w-1/3">
         Login with Spotify
       </button>
-<<<<<<< HEAD
       <button
        
         className=" text-lg font-semibold text-white  bg-black bg-opacity-40 hover:bg-opacity-20 rounded-lg  transition-all duration-300 font-atkinson-hyperlegible px-5 py-4
@@ -33,9 +25,6 @@ export default function Login() {
       </button>
       </div>
       
-=======
-      </div>
->>>>>>> ba131c55c1199d6f359d84dfe2a5f075b6cdabb3
     </div>
   );
 }
