8d917cdb8d24e89ba810754b185e93a3f978ad37
retekant <retekant@gmail.com>
2025-03-17 20:28:42 -0700

    added spotify logo and able to go to link from that logo as well

--- components/MiddleImageyTitle.tsx
+++ components/MiddleImageyTitle.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface middleImageyTitleProps {
   playerState: any;
@@ -17,11 +18,27 @@ export const MiddleImageyTitle : React.FC<middleImageyTitleProps> = ({
   getColor,
   handleClick
 }) => {
-    return <div className=' flex flex-col items-center justify-center h-full'>
+    return <div className=' flex flex-col items-center h-full'>
+      <div className='group flex items-center cursor-pointer mt-10 mb-3' style={{ width: imgRef.current?.width || 'auto' }}>
+        <Image 
+        src={txtColor === 'text-black' ? '/Spotify_Primary_Logo_RGB_Black.svg' : '/Spotify_Primary_Logo_RGB_White.svg'}
+        alt="Spotify Logo"
+        width={30}
+        height={30}
+        className={`${colorTransition}  `}
+          />
+
+          <div className={`overflow-hidden w-0 group-hover:w-24 transition-all duration-300 ${txtColor} ${colorTransition} ml-2`}>
+            <div className='whitespace-nowrap translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-300 underline'
+            onClick={handleClick}>To song link</div>
+          </div>
+      </div>
+
     <img ref={imgRef} src={playerState.item.album.images[0].url} 
-      className='rounded-xl mt-16 shadow-xl hover:scale-105 transition-transform duration-300 ease-in-out h-2/3 w-auto'
+      className='rounded-xl shadow-xl h-2/3 w-auto'
       onLoad={getColor} crossOrigin="anonymous"
-      onClick={handleClick} />
+       />
+       
     <div className='mt-5 text-center'>
      <div className={`text-5xl font-atkinson-hyperlegible p-3 ${txtColor} ${colorTransition}`}>
         {playerState.item.name}
